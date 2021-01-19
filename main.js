96628391faf3008361c31793627275b7989e4fed
Nika <tanny.spynu@gmail.com>
2021-01-19 18:13:11 +0200

    insarcinari - 3

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
-  let a = prompt();
+//------- 1 ------------------------
 
-  switch (a){
-     case '0': console.log('Zero');
-     break;
-     case '1': console.log('Unu');
-     break;
-     case '2': console.log('Doi');
-     break;
-     case '3': console.log('Trei');
-     break;
-     case '4': console.log('Patru');
-     break;
-     case '5': console.log('Cinci');
-     break;
-     case '6': console.log('Sase');
-     break;
-     case '7': console.log('Sapte');
-     break;
-     case '8': console.log('Opt');
-     break;
-     case '9': console.log('Noua');
-     break;
+ /* let arr = [1, 2, 3, 4 , 5, 6, 7];
+  let a = 0;
+  for (let i of arr){
+        if ((i%2) == 0){
+          a = a + i;  
+          
+      }     
   }
+  console.log(a);
+*/
 
-})
+//---------- 2 ----------------
+
+   /*let number = Number(prompt());
+
+   if (number > 0){
+       alert('1')
+   } else if (number < 0){
+       alert('-1')
+   } else if (number == 0){
+       alert('0')
+   } else{
+       alert(' nu este numar')
+   }*/
+
+//------------ 3 -------------------
+
+
+
+let styles = ["Jaz", "Blues"];
+console.log(styles)
+
+styles.push('Rock-n-Roll');
+console.log(styles)
+
+styles.splice(styles.length/2, 1, "Classica");
+console.log(styles)
+
+let shifted_item = styles.shift();
+console.log(styles)
+
+styles.unshift('Rap', 'Reggy')
+
+console.log(styles)
+console.log(shifted_item)
+
+}) 
