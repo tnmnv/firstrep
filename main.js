f0ca91c621066715655cd3474229e14357c23ed4
Nika <tanny.spynu@gmail.com>
2021-01-19 15:36:13 +0200

    insarcinare

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+  let a = prompt();
+
+  switch (a){
+     case '0': console.log(' a = 0');
+     break;
+     case '1': console.log(' a = 1');
+     break;
+     case '2': console.log(' a = 2');
+     break;
+     case '3': console.log(' a = 3');
+     break;
+     case '4': console.log(' a = 4');
+     break;
+     case '5': console.log(' a = 5');
+     break;
+     case '6': console.log(' a = 6');
+     break;
+     case '7': console.log(' a = 7');
+     break;
+     case '8': console.log(' a = 8');
+     break;
+     case '9': console.log(' a = 9');
+     break;
+  }
+
+})
