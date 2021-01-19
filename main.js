3209de88fe91f1b832089814b4c189bf2a15bfc3
Nika <tanny.spynu@gmail.com>
2021-01-19 18:45:58 +0200

    Tema pe acasa

--- main.js
+++ main.js
@@ -1,51 +1,42 @@
 document.addEventListener('DOMContentLoaded', () =>{
+ 
 
-//------- 1 ------------------------
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
- /* let arr = [1, 2, 3, 4 , 5, 6, 7];
-  let a = 0;
-  for (let i of arr){
-        if ((i%2) == 0){
-          a = a + i;  
-          
-      }     
-  }
-  console.log(a);
-*/
+    /*for (let i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }*/
 
-//---------- 2 ----------------
+    /*let i=0;
+    while(i < arr.length){
+        console.log(arr[i])
+        i++;
+    }*/
 
-   /*let number = Number(prompt());
+    /*let i=0;
+     do{
+        console.log(arr[i])
+        i++;}
+    while(i < arr.length)*/
 
-   if (number > 0){
-       alert('1')
-   } else if (number < 0){
-       alert('-1')
-   } else if (number == 0){
-       alert('0')
-   } else{
-       alert(' nu este numar')
-   }*/
+   /* for (let i in arr){
+        console.log(arr[i])
+    }*/
+    
+    /*let i ;
+    for (i of arr){
+        console.log(i)
+    }*/
 
-//------------ 3 -------------------
+    /* arr.forEach(myFunc)
+    function myFunc(val){
+    console.log(val)
+    }*/
 
+     
+     let myFunc = val => console.log(val)
+     arr.map(myFunc)
 
-
-let styles = ["Jaz", "Blues"];
-console.log(styles)
-
-styles.push('Rock-n-Roll');
-console.log(styles)
-
-styles.splice(styles.length/2, 1, "Classica");
-console.log(styles)
-
-let shifted_item = styles.shift();
-console.log(styles)
-
-styles.unshift('Rap', 'Reggy')
-
-console.log(styles)
-console.log(shifted_item)
-
+     let alta_metoda = arr.join();
+     console.log(alta_metoda)
 }) 
